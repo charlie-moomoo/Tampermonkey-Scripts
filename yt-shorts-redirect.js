38db8fb843f7536cbb50a5e045290088ed34f833
Charlie <kiwichang2017@gmail.com>
2022-10-22 20:09:34 +0800

    Update yt-shorts-redirect.js

--- yt-shorts-redirect.js
+++ yt-shorts-redirect.js
@@ -1,18 +1,20 @@
 // ==UserScript==
 // @name         YouTube Shorts Redirector
 // @namespace    https://github.com/charlie-moomoo
-// @version      0.2
+// @version      0.3
 // @description  Auto redirects /shorts link to /watch?v= link on YouTube.
 // @author       CharlieMoomoo
-// @match        *://youtube.com/shorts/*
-// @match        *://www.youtube.com/shorts/*
+// @match        *://*.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
-// @grant        none
+// @grant        window.onurlchange
 // @license      MIT
 // @run-at       document-start
 // ==/UserScript==
- 
+
 (function() {
     'use strict';
-    return window.location.replace(`https://youtube.com/watch?v=${location.href.split("/shorts/")[1]}`);
+    window.addEventListener("urlchange",()=>{
+      if (!location.pathname.startsWith("/shorts")) return;
+      return window.location.replace(`https://youtube.com/watch?v=${location.href.split("/shorts/")[1]}`);
+    });
 })();
