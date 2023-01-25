9ee43d0f50eb65bdbff8702aed3e73f87e1e5a87
Charlie <kiwichang2017@gmail.com>
2023-01-25 21:14:46 +0800

    Create yt-custom-country-code.js

---
+++ yt-custom-country-code.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Custom YouTube Country Code
+// @namespace    https://github.com/charlie-moomoo
+// @version      0.1
+// @description  Replace the country code next to the logo with anything you want.
+// @author       CharlieMoomoo
+// @match        *://*.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        window.onurlchange
+// @license      MIT
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const countrycode = "sus" //Replace it with anything you want
+    console.log("hi")
+    const replace = ()=>{
+        document.querySelectorAll('#country-code').forEach((element)=>{
+            element.innerHTML = countrycode
+        })
+    }
+    replace()
+    window.addEventListener("urlchange",replace);
+})();
